7874a94bde0453e1c7a6f2e19f7e1e6ffa08990c
kitsuneb1 <119156212+fogyfox@users.noreply.github.com>
2026-01-19 22:07:07 +0700

    Update calc.cpp

--- calc.cpp
+++ calc.cpp
@@ -1,127 +1,155 @@
 #include <iostream>
 #include <string>
 #include <cctype>
-#include <stdexcept>
+#include <cstdlib>
 
 using namespace std;
 
-string expr;
-size_t pos = 0;
+struct NodeS {
+    string key;
+    NodeS* next;
+};
 
-void skipSpaces() {
-    while (pos < expr.length() && isspace(expr[pos])) {
-        pos++;
+struct stack {
+    NodeS* head;
+    int size;
+};
+
+void init(stack& s) {
+    s.head = nullptr;
+    s.size = 0;
+}
+
+void push(stack& s, string val) {
+    NodeS* newNode = new NodeS;
+    newNode->key = val;
+    newNode->next = s.head;
+    s.head = newNode;
+    s.size++;
+}
+
+string pop(stack& s) {
+    if (s.head == nullptr) return "";
+    NodeS* temp = s.head;
+    string val = temp->key;
+    s.head = s.head->next;
+    delete temp;
+    s.size--;
+    return val;
+}
+
+string top(stack& s) {
+    if (s.head == nullptr) return "";
+    return s.head->key;
+}
+
+bool isEmpty(stack& s) {
+    return s.size == 0;
+}
+
+void clear(stack& s) {
+    while (!isEmpty(s)) {
+        pop(s);
     }
 }
 
-long long parseExpression();
+int priority(string op) {
+    if (op == "+" || op == "-") return 1;
+    if (op == "*" || op == "/") return 2;
+    return 0;
+}
 
-long long parseNumber() {
-    skipSpaces();
+void performOp(stack& nums, stack& ops) {
+    if (isEmpty(nums)) throw "Error";
+    long long b = stoll(pop(nums));
     
-    if (pos >= expr.length() || !isdigit(expr[pos])) {
-        throw runtime_error("-1");
-    }
+    if (isEmpty(nums)) throw "Error";
+    long long a = stoll(pop(nums));
+
+    string op = pop(ops);
 
     long long res = 0;
-    while (pos < expr.length() && isdigit(expr[pos])) {
-        res = res * 10 + (expr[pos] - '0');
-        pos++;
-    }
-    return res;
-}
-
-long long parseFactor() {
-    skipSpaces();
-    
-    if (pos >= expr.length()) {
-        throw runtime_error("-1");
-    }
-
-    if (expr[pos] == '(') {
-        pos++;
-        long long res = parseExpression();
-        skipSpaces();
-        if (pos >= expr.length() || expr[pos] != ')') {
-            throw runtime_error("-1");
-        }
-        pos++;
-        return res;
+    if (op == "+") res = a + b;
+    else if (op == "-") res = a - b;
+    else if (op == "*") res = a * b;
+    else if (op == "/") {
+        if (b == 0) throw "Error";
+        res = a / b;
     }
     
-    if (expr[pos] == '-') {
-        pos++;
-        return -parseFactor();
-    }
-    
-    if (expr[pos] == '+') {
-        pos++;
-        return parseFactor();
-    }
-
-    return parseNumber();
-}
-
-long long parseTerm() {
-    long long lhs = parseFactor();
-
-    skipSpaces();
-    while (pos < expr.length() && (expr[pos] == '*' || expr[pos] == '/')) {
-        char op = expr[pos];
-        pos++;
-        long long rhs = parseFactor();
-        if (op == '*') {
-            lhs *= rhs;
-        } else {
-            if (rhs == 0) {
-                throw runtime_error("-1");
-            }
-            lhs /= rhs;
-        }
-        skipSpaces();
-    }
-    return lhs;
-}
-
-long long parseExpression() {
-    long long lhs = parseTerm();
-
-    skipSpaces();
-    while (pos < expr.length() && (expr[pos] == '+' || expr[pos] == '-')) {
-        char op = expr[pos];
-        pos++;
-        long long rhs = parseTerm();
-
-        if (op == '+') {
-            lhs += rhs;
-        } else {
-            lhs -= rhs;
-        }
-        skipSpaces();
-    }
-    return lhs;
+    push(nums, to_string(res));
 }
 
 int main() {
+    string expr;
     getline(cin, expr);
 
+    stack nums;
+    init(nums);
+    
+    stack ops;
+    init(ops);
+
+    bool expectingOperand = true;
+
     try {
-        if (expr.empty()) {
-            throw runtime_error("-1");
+        for (int i = 0; i < expr.length(); i++) {
+            if (expr[i] == ' ') continue;
+
+            if (expr[i] == '(') {
+                push(ops, "(");
+                expectingOperand = true;
+            }
+            else if (isdigit(expr[i])) {
+                string numStr;
+                while (i < expr.length() && isdigit(expr[i])) {
+                    numStr += expr[i];
+                    i++;
+                }
+                i--;
+                push(nums, numStr);
+                expectingOperand = false;
+            }
+            else if (expr[i] == ')') {
+                while (!isEmpty(ops) && top(ops) != "(") {
+                    performOp(nums, ops);
+                }
+                if (isEmpty(ops)) throw "Error";
+                pop(ops);
+                expectingOperand = false;
+            }
+            else {
+                string currentOp(1, expr[i]);
+
+                if (currentOp == "-" && expectingOperand) {
+                    push(nums, "0");
+                    push(ops, "-");
+                } 
+                else {
+                    while (!isEmpty(ops) && priority(top(ops)) >= priority(currentOp)) {
+                        performOp(nums, ops);
+                    }
+                    push(ops, currentOp);
+                }
+                expectingOperand = true;
+            }
         }
 
-        pos = 0;
-        long long result = parseExpression();
-        
-        skipSpaces();
-        if (pos != expr.length()) {
-            throw runtime_error("-1");
+        while (!isEmpty(ops)) {
+            if (top(ops) == "(") throw "Error";
+            performOp(nums, ops);
         }
 
-        cout << result << endl;
-    } catch (const exception& e) {
+        if (nums.size != 1) throw "Error";
+
+        cout << pop(nums) << endl;
+
+    } catch (...) {
         cout << "Error" << endl;
     }
+
+    clear(nums);
+    clear(ops);
 
     return 0;
 }
